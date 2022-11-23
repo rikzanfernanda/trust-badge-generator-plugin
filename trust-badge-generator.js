ce16c5d63dbf169df399927dea1f03bb9e7cbafa
rikzan fernanda <fernandarikzan@gmail.com>
2022-11-23 15:44:55 +0700

    update layout

--- trust-badge-generator.js
+++ trust-badge-generator.js
@@ -1,3 +1,34 @@
-window.onload = function() {
-    
+window.onload = function () {
+    let btnGenerate = document.querySelector('#trust_badge_generator #btn-generate')
+    let modalElement = document.querySelector('#trust_badge_generator .trust_badge_generator-modal')
+    let tab1 = document.querySelector('#trust_badge_generator .tab-1')
+    let tab2 = document.querySelector('#trust_badge_generator .tab-2')
+    let btnWrapper = document.querySelector('#trust_badge_generator .btn-wrapper')
+
+    btnGenerate.addEventListener('click', function () {
+        modalElement.classList.remove("hidden")
+        tab1.classList.remove("hidden")
+        tab2.classList.add("hidden")
+        btnWrapper.classList.add("hidden")
+    })
+    document.querySelector('#trust_badge_generator .close').addEventListener('click', function () {
+        modalElement.classList.add("hidden")
+    })
+
+    let btnChooseImg = document.querySelectorAll("#trust_badge_generator .img-preview button")
+    btnChooseImg.forEach(function (btn) {
+        btn.addEventListener("click", function (e) {
+            let src = e.target.parentElement.querySelector("img").src
+
+            tab1.classList.add("hidden")
+            tab2.classList.remove("hidden")
+            btnWrapper.classList.remove("hidden")
+        })
+    })
+
+    document.querySelector('#trust_badge_generator #btnPrev').addEventListener('click', function () {
+        tab1.classList.remove("hidden")
+        tab2.classList.add("hidden")
+        btnWrapper.classList.add("hidden")
+    })
 }
--- trust-badge-generator.php
+++ trust-badge-generator.php
@@ -29,7 +29,97 @@ add_shortcode('trust-badge-generator', function ($atts) {
     wp_register_script('trust-badge-generator', plugin_dir_url(__FILE__) . 'trust-badge-generator.js?cache-breaker=' . time());
     wp_enqueue_script('trust-badge-generator');
 
-    return '<div id="trust_badge_generator">
-        
-    </div>';
+    return "
+        <div id='trust_badge_generator'>
+            <div class='text-center'>
+                <button id='btn-generate' class='bg-primary btn-rounded'>Click Here To Generate A Trust Badge</button>
+            </div>
+            <div class='trust_badge_generator-modal hidden'>
+                <div class='trust_badge_generator-modal-wrapper'>
+                    <div class='header'>
+                        <h4>Trust Badge Generator</h4>
+                        <button class='close'>×</button>
+                    </div>
+                    <div class='body'>
+                        <div class='tab-1'>
+                            <p class='text-center'>Click and choose a style first:</p>
+                            <div class='img-preview text-center'>
+                                <div>
+                                    <button><img src='". get_site_url() ."/wp-content/plugins/trust-badge-generator/previewimage/01-preview.png' width='350px'></button>
+                                </div>
+                                <div>
+                                    <button><img src='". get_site_url() ."/wp-content/plugins/trust-badge-generator/previewimage/02-preview.png' width='350px'></button>
+                                </div>
+                                <div>
+                                    <button><img src='". get_site_url() ."/wp-content/plugins/trust-badge-generator/previewimage/03-preview.png' width='350px'></button>
+                                </div>
+                                <div>
+                                    <button><img src='". get_site_url() ."/wp-content/plugins/trust-badge-generator/previewimage/04-preview.png' width='350px'></button>
+                                </div>
+                                <div>
+                                    <button><img src='". get_site_url() ."/wp-content/plugins/trust-badge-generator/previewimage/05-preview.png' width='350px'></button>
+                                </div>
+                                <div>
+                                    <button><img src='". get_site_url() ."/wp-content/plugins/trust-badge-generator/previewimage/06-preview.png' width='350px'></button>
+                                </div>
+                                <div>
+                                    <button><img src='". get_site_url() ."/wp-content/plugins/trust-badge-generator/previewimage/07-preview.png' width='350px'></button>
+                                </div>
+                                <div>
+                                    <button><img src='". get_site_url() ."/wp-content/plugins/trust-badge-generator/previewimage/08-preview.png' width='350px'></button>
+                                </div>
+                                <div>
+                                    <button><img src='". get_site_url() ."/wp-content/plugins/trust-badge-generator/previewimage/09-preview.png' width='350px'></button>
+                                </div>
+                                <div>
+                                    <button><img src='". get_site_url() ."/wp-content/plugins/trust-badge-generator/previewimage/10-preview.png' width='350px'></button>
+                                </div>
+                                <div>
+                                    <button><img src='". get_site_url() ."/wp-content/plugins/trust-badge-generator/previewimage/11-preview.png' width='350px'></button>
+                                </div>
+                                <div>
+                                    <button><img src='". get_site_url() ."/wp-content/plugins/trust-badge-generator/previewimage/12-preview.png' width='350px'></button>
+                                </div>
+                                <div>
+                                    <button><img src='". get_site_url() ."/wp-content/plugins/trust-badge-generator/previewimage/13-preview.png' width='350px'></button>
+                                </div>
+                                <div>
+                                    <button><img src='". get_site_url() ."/wp-content/plugins/trust-badge-generator/previewimage/14-preview.png' width='350px'></button>
+                                </div>
+                                <div>
+                                    <button><img src='". get_site_url() ."/wp-content/plugins/trust-badge-generator/previewimage/15-preview.png' width='350px'></button>
+                                </div>
+                                <div>
+                                    <button><img src='". get_site_url() ."/wp-content/plugins/trust-badge-generator/previewimage/16-preview.png' width='350px'></button>
+                                </div>
+                                <div>
+                                    <button><img src='". get_site_url() ."/wp-content/plugins/trust-badge-generator/previewimage/17-preview.png' width='350px'></button>
+                                </div>
+                                <div>
+                                    <button><img src='". get_site_url() ."/wp-content/plugins/trust-badge-generator/previewimage/18-preview.png' width='350px'></button>
+                                </div>
+                                <div>
+                                    <button><img src='". get_site_url() ."/wp-content/plugins/trust-badge-generator/previewimage/19-preview.png' width='350px'></button>
+                                </div>
+                                <div>
+                                    <button><img src='". get_site_url() ."/wp-content/plugins/trust-badge-generator/previewimage/20-preview.png' width='350px'></button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='tab-2 hidden'>
+                            <p class='text-center'>Click and choose a style first:</p>
+                            <input type='color' id='colorpick'>
+                            <div class='img-container'>
+                                <img id='foto-image' class='image' width='600px' />
+                            </div>
+                        </div>
+                        <div class='btn-wrapper hidden'>
+                            <button id='btnPrev' class='btn-rounded'>Previous</button>
+                            <button id='btnDownload' class='bg-primary btn-rounded'>Download</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    ";
 });
